4bf883d7b5fcd0fc0837da6cbd88e72924ae7dc4
carlosb114093 <carlos.botina@innovuslabs.com>
2026-07-04 00:13:53 -0500

    fix: use relative URLs and native events in user-profile spec

--- test/specs/user-profile.spec.js
+++ test/specs/user-profile.spec.js
@@ -4,17 +4,26 @@ should()
 describe('User Profile', () => {
     it('@user_profile - Customer edits their profile information', async () => {
         // 1. Given
-        await browser.url('https://practicesoftwaretesting.com/auth/login')
+        await browser.url('/auth/login')
 
         const emailObj = await $('[data-test="email"]')
-        await emailObj.click()
-        await emailObj.setValue('testuser3@epam.com')
-        await browser.keys('Tab')
+        await emailObj.waitForDisplayed({ timeout: 5000 })
+        await browser.execute((el, val) => {
+            const nativeSetter = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, 'value').set
+            nativeSetter.call(el, val)
+            el.dispatchEvent(new Event('input', { bubbles: true }))
+            el.dispatchEvent(new Event('change', { bubbles: true }))
+            el.dispatchEvent(new Event('blur', { bubbles: true }))
+        }, emailObj, 'testuser3@epam.com')
 
         const passwordObj = await $('[data-test="password"]')
-        await passwordObj.click()
-        await passwordObj.setValue('T3st12345@')
-        await browser.keys('Tab')
+        await browser.execute((el, val) => {
+            const nativeSetter = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, 'value').set
+            nativeSetter.call(el, val)
+            el.dispatchEvent(new Event('input', { bubbles: true }))
+            el.dispatchEvent(new Event('change', { bubbles: true }))
+            el.dispatchEvent(new Event('blur', { bubbles: true }))
+        }, passwordObj, 'T3st12345@')
 
         const loginBtn = await $('[data-test="login-submit"]')
         await loginBtn.click()
@@ -24,7 +33,7 @@ describe('User Profile', () => {
             { timeout: 12000, timeoutMsg: 'Login failed' }
         )
 
-        await browser.url('https://practicesoftwaretesting.com/account/profile')
+        await browser.url('/account/profile')
 
         // Wait until profile data is loaded
         const lnEl = await $('[data-test="last-name"]')
@@ -33,14 +42,15 @@ describe('User Profile', () => {
             { timeout: 8000, timeoutMsg: 'Profile data did not load' }
         )
 
-        // 2. When - change first name and submit
+        // 2. When - change first name using native events and submit
         const fnEl = await $('[data-test="first-name"]')
-        await fnEl.click()
-        await browser.keys(['Control', 'a'])
-        await browser.keys('Delete')
-        await browser.keys('Carlos')
-        await browser.keys('Tab')
-        await browser.pause(500)
+        await browser.execute((el, val) => {
+            const nativeSetter = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, 'value').set
+            nativeSetter.call(el, val)
+            el.dispatchEvent(new Event('input', { bubbles: true }))
+            el.dispatchEvent(new Event('change', { bubbles: true }))
+            el.dispatchEvent(new Event('blur', { bubbles: true }))
+        }, fnEl, 'Carlos')
 
         const updateBtn = await $('[data-test="update-profile-submit"]')
         await updateBtn.click()
